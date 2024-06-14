f391070b6e941e72f0ba0537af19c842f4ac4517
“[WEb <gouravsingla830@gmail.com>
2024-06-15 01:47:42 +0530

    Email Stuff

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,2 +1,22 @@
-import {resend}  from '../lib/resend'
+import { resend } from "../lib/resend";
+import VerificationEmail from "../../Emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+export async function sendVerification(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+from:'onboarding@resend.dev',
+to:email,
+subject:'Verify anonymous code | anonymous message',
+react:VerificationEmail({username,otp:verifyCode}),
+    })
 
+    return {success:true,message:' send verification Email'}
+  } catch (Eerror) {
+    console.log("Error sending verification Email",Eerror);
+    return {success:true,message:' send verification Email'}
+  }
+}
