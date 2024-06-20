70599423e97248e52bc23c6c243b4efd044b8aaf
“[WEb <gouravsingla830@gmail.com>
2024-06-21 00:52:03 +0530

    done defineing next user

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,3 +1,10 @@
 import "next-auth";
+import { usernameValidation } from "../schemas/signupSchema";
 
-declare module "next-auth" {}
+declare module "next-auth" {
+  interface User {
+    _id?: string;
+    isVerified?: boolean;
+    isAcceptingMessages?: boolean;
+    username?:string}
+}
