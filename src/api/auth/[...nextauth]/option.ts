102451c0e75bea2b5f6466d2c0aa14d34fd81afe
“[WEb <gouravsingla830@gmail.com>
2024-06-21 00:56:17 +0530

    defining session

--- src/api/auth/[...nextauth]/option.ts
+++ src/api/auth/[...nextauth]/option.ts
@@ -46,10 +46,16 @@ export const authOptions: NextAuthOptions = {
   ],
   callbacks: {
     async session({ session, token }) {
+      if(token){
+        session.user._id = token._id;
+      }
       return session;
     },
     async jwt({ token, user }) {
-      if (user) {token._id = user._id?.toStrig()
+      if (user) {token._id = user._id?.toString()
+        token.isVerified =user.isVerified;
+token.isAcceptingMessages=user.isAcceptingMessages;
+token.username=user.username;
       }
       return token;
     },
