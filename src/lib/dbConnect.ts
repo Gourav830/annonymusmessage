21bb276cd3c4072c79c1c8025e1bdeb134f42320
“[WEb <gouravsingla830@gmail.com>
2024-06-23 01:15:22 +0530

    db check

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -9,17 +9,15 @@ async function dbConnect(): Promise<void> {
   if (connection.isconnected) {
     console.log("connected to database");
   }
-  try{
-const db = await mongoose.connect(process.env.MONGO_URI || '')
-console.log(db);
-connection.isconnected = db.connections[0].readyState
-console.log("database connection success");
-
-
-}catch(err){
-    console.log("database connection failure" , err);
-process.exit(1)
-}
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/annomousMsg1')
+    // const db = await mongoose.connect('mongodb://127.0.0.1:27017/annomousMsg')
+    // console.log(db);
+    connection.isconnected = db.connections[0].readyState;
+    console.log("database connection success");
+  } catch (err) {
+    console.log("database connection failure", err);
+    process.exit(1);
+  }
 }
 export default dbConnect;
-
