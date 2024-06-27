0bf6672510129eee0117eef8b3fd1a8ae524cf96
“[WEb <gouravsingla830@gmail.com>
2024-06-28 02:19:27 +0530

    32

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -3,11 +3,12 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDebounceValue } from "usehooks-ts";
 import { useToast } from "@/components/ui/use-toast";
 import { useRouter } from "next/navigation";
 import { signupSchema } from "@/schemas/signupSchema";
+import axios from 'axios';
 const Page = () => {
   const [username, setUsername] = useState("");
   const [usernameMesssage, setUsernameMesssage] = useState("");
@@ -18,9 +19,20 @@ const Page = () => {
   const router = useRouter();
 
   const form = useForm({
-    resolver: zodResolver<z.infer <typeof signupSchema>>(signupSchema),
+    resolver: zodResolver<z.infer<typeof signupSchema>>(signupSchema),
     defaultValues: { username: "", email: "", password: "" },
   });
+  useEffect(() => {
+    const checkUsernameUniquie = async () => {
+      if (debouncedUsername) {
+        setIsCheckingMessage(true);
+        setUsernameMesssage("");
+        try {
+          await axios.get(`/api/check-username-unique? `)
+        } catch (error) {}
+      }
+    };
+  }, [debouncedUsername]);
   return (
     <div>
       <>
