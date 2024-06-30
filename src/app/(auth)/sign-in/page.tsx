b363793cef326be5b5447fd27aa1d8630a6b328d
“[WEb <gouravsingla830@gmail.com>
2024-07-01 01:54:19 +0530

    working on sign in

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,16 @@
+import { signinSchema } from '@/schemas/loginSchema';
+// import { signupSchema } from '@/schemas/signupSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 const Page = () => {
+
+  const form = useForm<z.infer<typeof signinSchema>>({
+    resolver: zodResolver(signinSchema),
+    defaultValues: {  identifier: "", password: "" },
+  });
   return (
     <div>
       
