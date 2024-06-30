ddf0f397043301b4eb98cb117ad7b28d3a108666
“[WEb <gouravsingla830@gmail.com>
2024-06-30 05:56:08 +0530

    working with verify route

---
+++ src/app/(auth)/verify/[...username]/page.tsx
@@ -0,0 +1,17 @@
+import { useToast } from '@/components/ui/use-toast';
+import { useParams, useRouter } from 'next/navigation';
+import React from 'react';
+
+const Page = () => {
+    const router = useRouter();
+    const param = useParams<{username:string}>();
+    const {toast} = useToast();
+    
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Page;
