a9df4d3ded403e82541c7c64818c620a92d92422
“[WEb <gouravsingla830@gmail.com>
2024-07-06 01:04:09 +0530

    navbar error

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,4 +1,5 @@
-import Navbar from '@/components/Navbar';
+import Navbar from '@/components/navbar';
+// import Navbar from '../../components/navbar';
 
 interface RootLayoutProps {
   children: React.ReactNode;
