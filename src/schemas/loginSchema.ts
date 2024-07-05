ceab09507db7fffa7db311afdb2ba278038a081e
“[WEb <gouravsingla830@gmail.com>
2024-07-06 00:55:18 +0530

    merge with xyz

--- src/schemas/loginSchema.ts
+++
@@ -1,4 +0,0 @@
-import { z } from "zod";
-
-export const signinSchema = z.object({identifier:z.string(),
-    password:z.string()});
--- src/types/ApiResponse.ts
+++
@@ -1,8 +0,0 @@
-
-import { Message } from "@/model/user";
-export interface ApiResponse {
-  success: boolean,
-  message: string,
-    isAcceptingMessage?:boolean,
-    messages?:Array<Message>
-}
